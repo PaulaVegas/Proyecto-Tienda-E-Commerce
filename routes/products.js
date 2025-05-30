f0a23bbc73fabc8e38315a4fb80d5800cddff5b4
Paula <mv.paula@gmail.com>
2025-05-30 16:47:46 +0200

    readme and added authorization to product endpoints

--- routes/products.js
+++ routes/products.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const ProductController = require('../controllers/ProductController');
+const { authentication, isAdmin } = require('../middlewares/authentication');
 
-router.post('/', ProductController.createProduct);
-router.put('/:id', ProductController.update);
-router.delete('/:id', ProductController.delete);
+router.post('/', authentication, isAdmin, ProductController.createProduct);
+router.put('/:id', authentication, isAdmin, ProductController.update);
+router.delete('/:id', authentication, isAdmin, ProductController.delete);
 router.get('/', ProductController.getAll);
 router.get('/search/name/:name', ProductController.searchProductByName);
 router.get('/search/price/:price', ProductController.searchProductByPrice);
